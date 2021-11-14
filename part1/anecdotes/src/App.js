245233ac957d1e606adc09f935da5ee8080c2846
pattar314 <patsdroid18@gmail.com>
2021-11-13 19:51:05 -0700

    1.14

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -2,7 +2,12 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-
+  const [selected, setSelected] = useState(Math.floor(Math.random())* 10)
+  const [points, setPoints] = useState({0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0})
+  const [highVote, setHighVote] = useState(0)
+  const [highAnecdote, setHighAnecdote] = useState(5)
+  
+  
   const anecdotes = [
     'If it hurts, do it more often',
     'Adding manpower to a late software project makes it later!',
@@ -13,18 +18,32 @@ function App() {
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
   ]
 
-  const [selected, setSelected] = useState(Math.floor(Math.random())* 10)
+  const handleVote = () => {
+    let copy = {...points}
+    copy[selected] += 1
+    setPoints(copy)
+    if(copy[selected] > highVote){
+      setHighVote(copy[selected])
+      setHighAnecdote(selected)
+    }
+  }
 
   const rollRandom = () =>  {
     setSelected(Math.floor(Math.random() * 6))
-    console.log('selected: ', selected)
   }
+
 
 
   return (
     <div className="App">
+      <h1>Anecdote of the day</h1>
       <div>{anecdotes[selected]}</div>
+      <div className='quoteVotes'>This quote has {points[selected]} votes</div>
+      <button onClick={() => handleVote()}>Vote</button> 
       <button onClick={() => rollRandom()}>Next anecdote</button>
+      <h1>Anecdote with the most votes</h1>
+      {anecdotes[highAnecdote]}
+
     </div>
   );
 }
