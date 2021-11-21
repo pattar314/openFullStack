a1d9271459b9eb933897074b5ddf9cf0d1ed1d60
pattar314 <patsdroid18@gmail.com>
2021-11-20 22:33:11 -0700

    2.14 finally

--- part2/country-data/src/App.js
+++ part2/country-data/src/App.js
@@ -7,15 +7,28 @@ function App() {
 
   const [search, setSearch] = useState('')
   const [searchResults, setSearchResults] = useState('')
+  const [weather, setWeather] = useState('')
+
+  let api_key = process.env.REACT_APP_WEATHER_API_KEY
+   const fetchWeather = async (country) => {
+      const params = {access_key: api_key, query: `${country.capital}`, units: 'f'}
+      try{
+      let data = await axios.get(`http://api.weatherstack.com/current`, {params}).then(response => response.data.current)
+        console.log('weather data: ', data)
+      return data
+      } catch (err){ console.log('there was an outer error: ', err) }
+      
+   }
+ 
 
   const handleSearchChange = (event) => {
     setSearchResults('')
-    let data;
     setSearch(event.target.value)
     let searchUrl = `https://restcountries.com/v3.1/name/${event.target.value}`;
     if (search !== ''){
       try{
-        axios.get(searchUrl).then((response) => {
+        let data = axios.get(searchUrl)
+        .then((response) => {
           data = response.data;
           (data.length === 1 ? displaySingleCountry(data[0]) : (data.length < 10 && search !== '' ? displayMultipleCountries(data) : console.log('Please enter more of the country name')))})
           .catch(error => console.log('there was an error: ', error.message))
@@ -23,11 +36,17 @@ function App() {
       }catch(error){console.log('there was an error: ', error.message)}
     }}
   
-  const displaySingleCountry = (country) => {
+  const displaySingleCountry = async (country) => {
+      let data = await fetchWeather(country)
+      setWeather(<>
+          <img src={data.weather_icons} alt='weather' />
+          <div>the temperature in {country.capital} is {data.temperature} and the wind is {data.wind_speed} {data.wind_dir} </div>
+        </>);
       let languages = []
       for(let lang in country.languages){
             languages.push(<li key={lang}>{country.languages[lang]}</li>)}
-      setSearchResults(<div>
+      setSearchResults(
+      <div>
         <h2>{country.name.common}</h2>
           <br />
           <div>Capital: {country.capital}</div>
@@ -36,6 +55,7 @@ function App() {
           <h3>Languages</h3>
           <ul>{languages}</ul>
           <div style={{fontSize: '3em'}}  >{country.flag}</div>
+          <h3>Weather in {country.capital}</h3>
       </div>
       )
   }
@@ -48,6 +68,7 @@ function App() {
     <div className="App">
       <div>Find countries:  <input onChange={handleSearchChange} type="text" value={search} /> </div>
       <div>{searchResults}</div>
+      <div>{weather}</div>
     </div>
   );
 }
