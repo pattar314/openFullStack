444e815e6c83461ee4f127b528102d105831b92f
pattar314 <patsdroid18@gmail.com>
2022-03-30 03:11:10 -0600

    part5 emptied

--- part5/blog/bloglistFrontend/src/services/blogs.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-const baseUrl = '/api/blogs'
-
-const getAll = async () => {
-  const request = await axios.get(baseUrl)
-  return request.data
-}
-
-export default { getAll }
