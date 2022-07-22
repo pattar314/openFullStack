6d9176e23b66277950159a9fbf1a4b8fbc5c16ec
pattar314 <patsdroid18@gmail.com>
2022-07-22 02:02:13 -0600

    ready for final exercises of part 6

--- part6/reduxNotes/notesFrontend/src/components/NewNoteForm.js
+++ part6/reduxNotes/notesFrontend/src/components/NewNoteForm.js
@@ -1,18 +1,15 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
 import { createNote } from '../reducers/noteReducer'
-// import noteService from '../services/noteServices'
+import { connect } from 'react-redux'
 
-const NewNoteForm = () => {
-  const dispatch = useDispatch()
 
-  const addNote = async (event) => {
+const NewNoteForm = (props) => {
+
+  const addNote = (event) => {
     event.preventDefault()
     const content = event.target.note.value
     event.target.note.value = ''
-    // if issues look here
-    // const newNote = await noteService.createNew(content)
-    dispatch(createNote(content))
+    props.createNote(content)
   }
   
 
@@ -24,4 +21,7 @@ const NewNoteForm = () => {
   )
 }
 
-export default NewNoteForm
+export default connect(
+  null,
+  { createNote }
+  )(NewNoteForm)
